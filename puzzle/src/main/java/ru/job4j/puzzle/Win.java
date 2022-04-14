bbfe2808f1eaaddc4d9823d9a9ccb78c0de9c6f9
feyour <feyour@gmail.com>
2022-04-14 23:33:09 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,35 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int index = 0; index < board.length; index++) {
+            if (Horizontal(board, index) || Vertical(board, index)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean Horizontal(int[][] board, int row) {
         boolean rsl = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[row][index] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean Vertical(int[][] board, int cell) {
+        boolean rsl = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][cell] != 1) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
